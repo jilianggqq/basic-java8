0c34f5d3888d73e8e183a6f897e4d2432ed53971
Qiqiang <guan5678xyz@gmail.com>
2022-03-13 10:24:15 -0700

    algorithm

---
+++ basic-java8-core/src/main/java/edu/gqq/ayuan/week7/Deque.java
@@ -0,0 +1,77 @@
+package edu.gqq.ayuan.week7;
+
+import java.util.ArrayList;
+
+/**
+ * File Name: DequeTest.java
+ *
+ *
+ * To Compile: IntUtil.java RandomInt.java Deque.java DequeTest.java
+ *
+ * @author Jagadeesh Vasudevamurthy
+ * @year 2022
+ */
+class Deque {
+    //You can use any data structure you want except Deque like ArrayDeque from Java library
+    //All operation must be O(1)
+
+    Deque() {
+
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     */
+    public void addFirst(Integer v) {
+
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     */
+    public void addLast(Integer v) {
+
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     */
+    public int removeFirst() {
+        return 0; //FILL
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     */
+    public int removeLast() {
+        return 0; //FILL
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     */
+    public int size() {
+        return 0; //FILL
+    }
+
+    /*
+     * MUST BE Time:O(1)
+     * MUST BE Space O(1)
+     * THIS IS THE HEART OF MY INFO 6205
+     * Can you get item at 'pos' in time O(1) and space O(1)
+     * For 100% grade, this must work
+     */
+    public int getV(int pos) {
+        return 0; //FILL
+    }
+
+    /*************************************************************
+     * ALL PRIVATE ROUTINES BELOW
+     **************************************************************/
+
+}
